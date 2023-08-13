4aeea980099cdf93adccbef05529ff3c62a18c09
Peter-Easterbrook <peter.easterbrook@beka-software.at>
2023-08-13 19:47:19 +0200

    Adds framer motion to nav

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,8 +1,30 @@
+import { motion } from 'framer-motion';
+import { useState } from 'react';
 import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
 import { NavLink } from 'react-router-dom';
 import classes from './MainNavigation.module.css';
 
+const LINKS = [
+  {
+    slug: 'quotes',
+    label: 'All Quotes',
+    to: '/quotes',
+  },
+  {
+    slug: 'new-quote',
+    label: 'Add Quote',
+    to: '/new-quote',
+  },
+  {
+    slug: 'login',
+    label: 'Login',
+    href: '/login',
+  },
+];
+
 const MainNavigation = () => {
+  const [hoveredNavItem, setHoveredNavItem] = useState(null);
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>
@@ -15,18 +37,30 @@ const MainNavigation = () => {
         </span>
       </div>
 
-      <nav className={classes.nav}>
+      <nav className={classes.nav} onMouseLeave={() => setHoveredNavItem(null)}>
         <ul>
-          <li>
-            <NavLink to='/quotes' className={classes.btn}>
-              All Quotes
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/new-quote' className={classes.btn}>
-              Add a Quote
-            </NavLink>
-          </li>
+          {LINKS.map(({ slug, label, to }) => (
+            <li
+              key={slug}
+              style={{
+                zIndex: hoveredNavItem === slug ? 1 : 2,
+              }}
+            >
+              {hoveredNavItem === slug && (
+                <motion.div
+                  layoutId='hovered'
+                  className={classes.hovered}
+                  initial={{
+                    borderRadius: 32,
+                  }}
+                />
+              )}
+
+              <NavLink to={to} onMouseEnter={() => setHoveredNavItem(slug)}>
+                {label}
+              </NavLink>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
