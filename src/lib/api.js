25c7be5c369f576abb69c37433b9e462348b62a4
Peter-Easterbrook <peter.easterbrook@beka-software.at>
2023-08-17 18:58:04 +0200

    Adds .env variable

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const FIREBASE_DOMAIN = 'https://react-hooks-update-9b87b.firebaseio.com';
+const FIREBASE_DOMAIN = process.env.REACT_APP_FIREBASE_DOMAIN;
 
 export async function getAllQuotes() {
   const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`);
